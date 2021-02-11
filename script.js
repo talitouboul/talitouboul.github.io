061053b4a51fe2112585d6912cd323ce2dc3fedb
talitouboul <talitouboul@hotmail.fr>
2021-02-11 10:24:17 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var typed = new Typed('#typed', {
     // Attend 1000 ms après avoir tapé "First"
-    chaînes : ['tali', 'dev', 'knecr', 'ihrhrv'],
+    chaînes: ['tali', 'dev', 'knecr', 'ihrhrv'],
     typeSpeed: 100,
     loop: true,
-}) ;
+});
